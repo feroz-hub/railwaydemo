80aea03fbfa06fbbcb68e09233776430341b11bc
feroze <bashaferoz66@gmail.com>
2026-02-19 06:43:24 +0530

    Docker added

--- RailwayDemoApi/Program.cs
+++ RailwayDemoApi/Program.cs
@@ -3,8 +3,12 @@ using RailwayDemoApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get connection string from Railway's environment variable
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") 
+                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
