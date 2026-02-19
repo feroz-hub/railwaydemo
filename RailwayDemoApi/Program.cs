d09e08577587b37d9115d296f945360019672015
feroze <bashaferoz66@gmail.com>
2026-02-19 09:58:22 +0530

    Enhance database connection handling and migration error logging

--- RailwayDemoApi/Program.cs
+++ RailwayDemoApi/Program.cs
@@ -3,13 +3,31 @@ using RailwayDemoApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Get connection string from Railway's environment variable
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") 
-                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+string? connectionString;
+
+if (string.IsNullOrEmpty(databaseUrl))
+{
+    // Local development
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Parse Railway's URI format
+    var databaseUri = new Uri(databaseUrl);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    connectionString = $"Host={databaseUri.Host};" +
+                       $"Port={databaseUri.Port};" +
+                       $"Database={databaseUri.AbsolutePath.TrimStart('/')};" +
+                       $"Username={userInfo[0]};" +
+                       $"Password={userInfo[1]};" +
+                       $"SSL Mode=Require;Trust Server Certificate=true;";
+}
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,9 +38,22 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+// In Program.cs after app.Build()
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        if (context.Database.IsRelational())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
 }
 app.Run();
